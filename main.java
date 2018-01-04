70b6fd4f69e5a0db1f414b0eda4fec71329ee9a2
Emmett Ling <emmettl@berkeley.edu>
2018-01-04 00:27:45 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+Well, nothing much here....
+
+Framework is as follows:
+Step 1: Build a messenger bot
+Step 2: Build a page reader
+Step 3: Plug them into each other and start running a scan server
+Step 4: ????????
+Step 5: Profit
+
+
+Some links which I've been reading to figure out how to do all this stuff
+https://stackoverflow.com/questions/9825798/how-to-read-a-text-from-a-web-page-with-java
+https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html
+https://developers.facebook.com/docs/messenger-platform
+https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start
+https://github.com/fbsamples/messenger-platform-samples/tree/tutorial-starters/quick-start
