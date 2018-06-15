a00d1af991f2de267324903b0a1241a451a12239
Emmett Ling <emmettl@berkeley.edu>
2018-06-15 11:21:40 -0700

    To be completed Winter Break 2018....

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 #Use HTML parser on FAFS.
 #For now, default to messaging one person. 
 #Hard code items, and notifications.  Currently no price cap, since we're aiming for "larger" items.
+#To be completed Winter Break 2018....
+@meltingmettle
+@senorryan
+@xX_surya_Xx
+@avad_the_frosh
 
+@cici_senor
+@wilsonw926
 
 #https://stackoverflow.com/questions/20045955/regex-pattern-in-python-for-parsing-html-title-tags
 #https://developers.facebook.com/docs/messenger-platform/reference/send-api/
