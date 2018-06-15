08dcc5fee232028d516de0629894a3fa6e86c341
Emmett Ling <emmettl@berkeley.edu>
2018-06-15 11:07:26 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#Switch to Python. 
+#Use HTML parser on FAFS.
+#For now, default to messaging one person. 
+#Hard code items, and notifications.  Currently no price cap, since we're aiming for "larger" items.
+
+
+#https://stackoverflow.com/questions/20045955/regex-pattern-in-python-for-parsing-html-title-tags
+#https://developers.facebook.com/docs/messenger-platform/reference/send-api/
